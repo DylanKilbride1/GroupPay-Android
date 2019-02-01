aa22a4660fa4c676d9395abe7280981d9205e50d
Dylan Kilbride <dylankilbride1@gmail.com>
2019-02-01 13:55:04 +0000

    UI Created for stage 1 of group creation

--- app/src/main/java/grouppay/dylankilbride/com/activities/Home.java
+++ app/src/main/java/grouppay/dylankilbride/com/activities/Home.java
@@ -69,9 +69,9 @@ public class Home extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-//        Intent intent = new Intent(Home.this, CreateAccount.class);
-//        intent.putExtra("userId", userId);
-//        startActivity(intent);
+        Intent intent = new Intent(Home.this, CreateGroupAccountNameImg.class);
+        intent.putExtra("userId", userId);
+        startActivity(intent);
       }
     });
 
