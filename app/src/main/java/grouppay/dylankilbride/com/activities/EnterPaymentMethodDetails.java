44eb0bd9a0f422fb1a59f14fffcd61a058d3c46b
Dylan Kilbride <dylankilbride1@gmail.com>
2019-05-31 22:26:25 +0100

    Began saved cards process on group deposit

--- app/src/main/java/grouppay/dylankilbride/com/activities/EnterPaymentMethodDetails.java
+++ app/src/main/java/grouppay/dylankilbride/com/activities/EnterPaymentMethodDetails.java
@@ -3,7 +3,12 @@ package grouppay.dylankilbride.com.activities;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import grouppay.dylankilbride.com.adapters.PaymentMethodsRVAdapter;
 import grouppay.dylankilbride.com.grouppay.R;
+import grouppay.dylankilbride.com.models.Cards;
 import grouppay.dylankilbride.com.models.StripeCharge;
 import grouppay.dylankilbride.com.models.StripeChargeReceipt;
 import grouppay.dylankilbride.com.retrofit_interfaces.CardManagerAPI;
@@ -11,12 +16,14 @@ import grouppay.dylankilbride.com.text_watchers.CardExpiryDateTextWatcher;
 import grouppay.dylankilbride.com.text_watchers.CardNumberTextWatcher;
 import io.card.payment.CardIOActivity;
 import io.card.payment.CreditCard;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -32,6 +39,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,6 +49,14 @@ import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
 import com.stripe.android.model.Token;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static grouppay.dylankilbride.com.constants.Constants.LOCALHOST_SERVER_BASEURL;
 import static java.security.AccessController.getContext;
 
@@ -49,11 +65,15 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
   public final int MY_SCAN_REQUEST_CODE = 1234;
   private EditText cardholderName, cardNumber, expiryDate, cvv;
   private Button usePaymentDetailsBTN;
+  private TextView selectSavedPaymentMethod;
   private String expiryMonth, expiryYear, amountToDebitStr, amountForGroupStr, userId, groupAccountId;
   private double amountToDebit, amountForGroup;
   private ProgressWheel paymentProgressSpinner;
   private FrameLayout progressOverlay;
-  private CardManagerAPI cardManagerApiInterface;
+  private CardManagerAPI apiInterface;
+  private ArrayList<Cards> paymentMethodsList;
+  private RecyclerView paymentMethodsRecyclerView;
+  private RecyclerView.LayoutManager paymentMethodsRecyclerViewLayoutManager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +87,10 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
     amountToDebit = Double.parseDouble(amountToDebitStr);
     amountForGroup = Double.parseDouble(amountForGroupStr);
 
+    paymentMethodsList = new ArrayList<>();
+
+    paymentMethodsRequestSetUp(userId);
+
     setUpActionBar();
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -77,6 +101,15 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
     usePaymentDetailsBTN = (Button) findViewById(R.id.enterPaymentMethodDetailsContinueBTN);
     paymentProgressSpinner = (ProgressWheel) findViewById(R.id.progressWheel);
     progressOverlay = (FrameLayout) findViewById(R.id.progress_overlay);
+    selectSavedPaymentMethod = findViewById(R.id.enterPaymentDetailsSelectSavedDetails);
+
+    if(checkPaymentMethodsListSize() > 0) {
+      selectSavedPaymentMethod.setVisibility(View.VISIBLE);
+
+      selectSavedPaymentMethod.setOnClickListener(view -> {
+        showSavedCardsDialog();
+      });
+    }
 
     usePaymentDetailsBTN.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -89,6 +122,19 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
     expiryDate.addTextChangedListener(new CardExpiryDateTextWatcher());
   }
 
+  public void setUpSavedCardsRecyclerView(List<Cards> cardsList) {
+    // set up the RecyclerView
+    paymentMethodsRecyclerView = (RecyclerView) findViewById(R.id.paymentMethodsRV);
+    paymentMethodsRecyclerViewLayoutManager = new LinearLayoutManager(this);
+    paymentMethodsRecyclerView.setLayoutManager(paymentMethodsRecyclerViewLayoutManager);
+    paymentMethodsRecyclerView.setAdapter(new PaymentMethodsRVAdapter(cardsList, R.layout.saved_cards_dialog_list_item));
+    paymentMethodsRecyclerView.addItemDecoration(new DividerItemDecoration(paymentMethodsRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
+
+  }
+
+  private int checkPaymentMethodsListSize() {
+    return paymentMethodsList.size();
+  }
 
   private void createPaymentCardToken(){
     Stripe stripe = new Stripe(this, "pk_test_kwfy65ynBeJFLDiklvYHV2tI00fUxcehhP");
@@ -135,13 +181,13 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
         .baseUrl(LOCALHOST_SERVER_BASEURL)
         .addConverterFactory(GsonConverterFactory.create())
         .build();
-      cardManagerApiInterface = sendToken.create(CardManagerAPI.class);
+      apiInterface = sendToken.create(CardManagerAPI.class);
     sendTokenToServer(stripeCharge);
   }
 
   private void sendTokenToServer(StripeCharge stripeCharge) {
     Call<Void> call;
-    call = cardManagerApiInterface.sendStripeTokenToServer(stripeCharge);
+    call = apiInterface.sendStripeTokenToServer(stripeCharge);
     call.enqueue(new Callback<Void>() {
       @Override
       public void onResponse(Call<Void> call, Response<Void> response) {
@@ -233,6 +279,14 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
     }
   }
 
+  private void showSavedCardsDialog() {
+//    setUpSavedCardsRecyclerView(paymentMethodsList);
+    final Dialog dialog = new Dialog(EnterPaymentMethodDetails.this);
+    dialog.setContentView(R.layout.saved_cards_dialog);
+
+    LinearLayout card = dialog.findViewById(R.id.savedCardLL);
+  }
+
 //  private void optionalCardSavingDialog() {
 //    AlertDialog.Builder adb = new AlertDialog.Builder(this);
 //    adb.setTitle("Pay and save card details?");
@@ -281,5 +335,61 @@ public class EnterPaymentMethodDetails extends AppCompatActivity {
       default:
         return super.onOptionsItemSelected(item);
     }
+  }
+
+  private void parsePaymentDetails(String stripeCardList) {
+    String hiddenDigits = "\u2022\u2022\u2022\u2022 \u2022\u2022\u2022\u2022 \u2022\u2022\u2022\u2022 ";
+    try {
+      JSONObject scl = new JSONObject(stripeCardList);
+      JSONArray dataArray = scl.getJSONArray("data");
+      for(int i = 0; i < dataArray.length(); i++) {
+        JSONObject paymentObject = dataArray.getJSONObject(i);
+        String protectedCardNumber = hiddenDigits + paymentObject.getString("last4");
+        paymentMethodsList.add(new Cards(protectedCardNumber,
+            paymentObject.getInt("exp_month"),
+            paymentObject.getInt("exp_year"),
+            paymentObject.getString("brand")));
+      }
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void paymentMethodsRequestSetUp(String userId) {
+    Retrofit getPaymentMethods = new Retrofit.Builder()
+        .baseUrl(LOCALHOST_SERVER_BASEURL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+    apiInterface = getPaymentMethods.create(CardManagerAPI.class);
+    getPaymentMethodsCall(userId); //TODO Change!
+  }
+
+  public void getPaymentMethodsCall(String userId) {
+    Call<ResponseBody> call = apiInterface.getUsersPaymentMethods(userId);
+    call.enqueue(new Callback<ResponseBody>() {
+      @Override
+      public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+        if(!response.isSuccessful()) {
+          //Handle
+        } else {
+          if(!response.body().toString().isEmpty()) {
+            paymentMethodsList.clear();
+            try {
+              parsePaymentDetails(response.body().string());
+              setUpSavedCardsRecyclerView(paymentMethodsList);
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          } else {
+            paymentMethodsList.clear();
+            setUpSavedCardsRecyclerView(paymentMethodsList);
+          }
+        }
+      }
+      @Override
+      public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+      }
+    });
   }
 }
